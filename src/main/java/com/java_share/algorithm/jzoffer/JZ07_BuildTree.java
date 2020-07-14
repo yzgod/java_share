d1b1444113684fc81d315025c24c1e8e5b2ab322
yz <919573416@qq.com>
2020-07-14 18:44:42 +0800

    剑指offer

--- src/main/java/com/java_share/algorithm/jzoffer/JZ07_BuildTree.java
+++ src/main/java/com/java_share/algorithm/jzoffer/JZ07_BuildTree.java
@@ -1,6 +1,8 @@
 package com.java_share.algorithm.jzoffer;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -21,13 +23,43 @@ public class JZ07_BuildTree {
         int[] preorder = {3,9,8,10,20,15,7};
         int[] inorder = {8,9,10,3,15,20,7};
         TreeNode tree = new Solution().buildTree(preorder, inorder);
-
+        System.out.println(tree);
     }
 
     static class Solution {
 
         public TreeNode buildTree(int[] preorder, int[] inorder) {
-            return null;
+            if (preorder == null || preorder.length == 0) {
+                return null;
+            }
+            int length = preorder.length;
+            Map<Integer, Integer> inMap = new HashMap<>(length);
+            for (int i = 0; i < length; i++) {
+                inMap.put(inorder[i], i);
+            }
+            TreeNode root = buildTree(preorder, 0, length - 1, 0, length - 1, inMap);
+            return root;
         }
+
+        public TreeNode buildTree(int[] pre, 
+                                  int ps, int pe, 
+                                  int is, int ie, 
+                                  Map<Integer, Integer> inMap) {
+            if (ps > pe) {
+                return null;
+            }
+            int val = pre[ps];
+            TreeNode head = new TreeNode(val);
+            if (ps != pe) {
+                int in = inMap.get(val);
+                int l = in - is, r = ie - in;
+                TreeNode left = buildTree(pre, ps + 1, ps + l, is, in - 1, inMap);
+                TreeNode right = buildTree(pre, pe - r + 1, pe, in + 1, ie, inMap);
+                head.left = left;
+                head.right = right;
+            }
+            return head;
+        }
+
     }
 }
