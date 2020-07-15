9c3b0866904aee712d53ec229fe4856da30e0d19
yz <919573416@qq.com>
2020-07-15 18:30:19 +0800

    jzoffer

---
+++ src/main/java/com/java_share/algorithm/jzoffer/JZ17_PrintNumbers.java
@@ -0,0 +1,27 @@
+package com.java_share.algorithm.jzoffer;
+
+/**
+ * @author yz
+ * @date 2020-07-15 13:39
+ * <br>
+ *      剑指 Offer 16. 数值的整数次方
+ *      实现函数double Power(double base, int exponent)，
+ *      求base的exponent次方。不得使用库函数，同时不需要考虑大数问题。
+ *
+ * </br>
+ * <br>
+ * </br>
+ **/
+public class JZ17_PrintNumbers {
+
+    public static void main(String[] args){
+
+    }
+
+    static class Solution {
+        public int[] printNumbers(int n) {
+
+            return null;
+        }
+    }
+}
